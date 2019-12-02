0d6349ada7140e65305e45aa16f3f899cd5c69b0
SuMetal97 <wrigley1@mail.usf.edu>
2019-12-02 15:47:28 -0500

    Final Commit

--- Main.cpp
+++ Main.cpp
@@ -1,32 +1,177 @@
 #include <stdio.h>
 #include <fstream>
-
+#include <iostream>
+#include <list>
+#include <stack>
+#include <string>
 using namespace std;
+int *totalmaze;
+int start_maze[3];
+int finish_maze[3];
+int maze_end_location;
+int maze[3];
+bool *visited;
+stack <string> s;
 
-struct node{
-    bool north = false;
-    bool south = false;
-    bool west = false;
-    bool east = false;
-    bool up = false;
-    bool down = false;
-    bool discovered = false;
-    struct node* UP;
-    struct node* DOWN;
-    struct node* WEST;
-    struct node* EAST;
-    struct node* NORTH;
-    struct node* SOUTH;
+//pointer struct for storing the next node;
+struct adjNode{
+    string id;
+    adjNode *next;
 };
 
-void createMaze(){
+void createMaze();
+
+/*this function takes in a location of the node and will check the given parameters on where it can traverse.
+if the node cannot find the end it returns NULL so the call before it can traverse as needed*/
+adjNode* find_end(int location){
+    adjNode *Node = new adjNode();
+    
+    Node->next = NULL;
+    if(visited[location]==true){
+        
+        return NULL;
+    }
+   
+    visited[location] = true;
+    if(location ==maze_end_location){
+        
+        visited[location] == false;
+        return Node;
+    }
+
+
+    if(totalmaze[location*6] == 1){
+        Node->next = find_end(location-maze[2]);
+        
+        if (Node->next!=NULL){
+            s.push("N");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+    if((totalmaze[location*6+1])==1){
+         Node->next = find_end(location+1);
+         
+      if(Node->next!=NULL){
+          s.push("E");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+     if((totalmaze[location*6+2])==1){
+         Node->next = find_end(location+maze[2]);
+         
+         if(Node->next!=NULL){
+             s.push("S");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+     if((totalmaze[location*6+3])==1){
+         Node->next = find_end(location-1);
+        
+        if(Node->next!=NULL){
+            s.push("W");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+     if((totalmaze[location*6+4])==1){
+         Node->next = find_end(location+(maze[2]*maze[1]));
+         
+         if(Node->next!=NULL){
+             s.push("U");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+     if((totalmaze[location*6+5])==1){
+         Node->next = find_end(location-(maze[1]*maze[2]));
+         
+        if(Node->next!=NULL){
+            s.push("D");
+            return Node;
+        }
+        Node->next = NULL;
+    }
+    
+    return NULL;
+
+}
+///////////////////////////////////////////////////////////////////////////////
+
+//main function////////////////////////////////////////
+int main(){
+    createMaze();    
+    return 0;
+}
+///////////////////////////////////////////////////////
+
+
+/*format the input file and call the traversing function
+This then outputs the needed info to the output file*/
+void createMaze()
+{
     ifstream input_file;
     ofstream output_file;
 
     input_file.open("input.txt");
+    output_file.open("output.txt");
     int maze_Num = 0;
-    input_file>>maze_Num;
+    input_file >> maze_Num;
     //setup input for the 6 ponts for start and finish
-    //make the matrix values stored in a giant 2d array and traverse that way with nodes of 6 and create the node
-    //check values of each node to see directions they can go
+
+    for(int pointer = 0; pointer < maze_Num; pointer++){
+    for (int i = 0; i < 3; i++)
+    {
+        input_file >> maze[i];
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        input_file >> start_maze[i];
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        input_file >> finish_maze[i];
+    }
+
+    totalmaze = new int[(maze[0] * maze[1] * maze[2] * 6)];
+    
+
+    visited = new bool[maze[0] * maze[1] * maze[2]];
+
+    for (int i = 0; i < maze[0] * maze[1] * maze[2]; i++)
+    {
+        int temp;
+        input_file >> temp;
+        visited[i] = false;
+        for (int j = 0; j < 6; j++)
+        {
+            int temp_value = temp % 2;
+
+            totalmaze[(i * 6) + 5 - j] = temp_value;
+            temp = temp / 10;
+        }
+    }
+
+    
+    
+    
+    int location = (start_maze[0]*maze[1]*maze[2]+ start_maze[1]*maze[2] + start_maze[2]);
+    maze_end_location = ((finish_maze[0])*maze[1]*maze[2] + (finish_maze[1])*maze[2] + (finish_maze[2]));
+    
+    adjNode *root = new adjNode();
+    root = find_end(location);
+
+    while (!s.empty()) { 
+        output_file << s.top() << " ";
+        s.pop(); 
+    } 
+    
+    output_file << "\n\n";
+
+    }
+
+    input_file.close();
+    output_file.close();
 }
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Itsy_Bitsy_Spider_Maze
+INTRODUCTION:
+This program will take in any matrix int the format of
+
+number of mazes
+
+maze_floors maze_rows maze_columns
+
+start_floor start_row start_column
+
+end_floor end_row end_column
+
+followed by each individual cells data in the directions you can traverse from that spot in this order:
+    North, East, South, West, Up, Down
+with 1 being it can move in that direction.
+
+you then repeat that format above for each maze(you only need the number of mazes at the top once);
+
+make sure the input file that you want to use for this program is in the same folder as Main.cpp and that it is titled "input.txt"
+
+TO COMPILE:
+    g++ -o matrix Main.cpp
+    ./matrix.exe
